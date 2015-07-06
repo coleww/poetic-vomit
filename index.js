2dd36c4c28676500fe27fbbb9dc37d38e8a648dd
Cole Willsea <coleww@gmail.com>
2015-07-05 18:45:02 -0700

    does the stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+var pronouncing = require('pronouncing')
+
+module.exports = function(text, lev){
+  var level = lev !== undefined ? lev : 0.5
+  return text.split(/\s/).map(function(word){
+    var punct = word.match(/\W+$/)
+    var searchWord = punct ? word.substr(0, punct.index) : word
+    var rhymes = pronouncing.rhymes(searchWord.toLowerCase())
+    var newWord = ''
+    if(rhymes.length && Math.random() < level){
+      var capitalized = word.charAt(0).toUpperCase() + word.slice(1) === word
+      var allCaps = word.toUpperCase() === word
+      newWord = rhymes[~~(Math.random() * rhymes.length)]
+      if(capitalized) newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1)
+      if(allCaps) newWord = newWord.toUpperCase()
+      if(punct) newWord += punct[0]
+    } else {
+      newWord = word
+    }
+    return newWord
+  }).join(" ")
+}
---
+++ test.js
@@ -0,0 +1,28 @@
+var tap = require('tape')
+var vomit = require('./')
+
+var string = 'the swift brown fox jumps over the cromulent lazy gopher face.'
+
+tap.test('replaces words with rhyming words', function(t){
+  t.equal(string.split(" ").length, vomit(string).split(" ").length)
+  t.end()
+})
+
+tap.test('maintains punctuation', function(t){
+  var puked = vomit(string)
+  t.equal('.', puked[puked.length - 1])
+  t.end()
+})
+
+tap.test('accepts level param', function(t){
+  t.equal(string, vomit(string, 0))
+  t.notEqual(string, vomit(string, 1))
+  t.end()
+})
+
+tap.test('maintains capitalization', function(t){
+  var capped = 'Oh ' + string
+  var res = vomit(capped)[0]
+  t.equal(res, res.toUpperCase())
+  t.end()
+})
